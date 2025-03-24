2d2c2eca767a57f262d64a8c454677d1bb8ca7cb
declan-whitty <155405085+declan-whitty@users.noreply.github.com>
2025-03-24 13:55:52 +1000

    structure Home page layout per wireframe and link featured listing to winter jacket

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,11 @@
 import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
 import Navbar from "../components/Navbar";
-import featuredImg from "../assets/featured-img.png";
+import featuredImg from "../assets/placeholder.png";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div className="page-wrapper">
-      {/* Green Header */}
-      <header className="home-header">
-        <h1>SwapSpot</h1>
-        <FaSearch />
-      </header>
-
-      {/* Sub-navigation bar */}
       <Navbar />
 
       <main className="main-content">
@@ -23,28 +15,18 @@ function Home() {
           <div className="featured-card">
             <img
               src={featuredImg}
-              alt="Featured"
+              alt="Winter Jacket"
               className="featured-image"
-              style={{
-                width: 300,
-                height: 300,
-                borderRadius: "50%",
-                objectFit: "cover",
-              }}
             />
             <div>
-              <p>
-                Lorem ipsum dolor sit amet. Et corrupti rerum in expedita animi
-                voluptatibus. Aut sunt doloribus. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Blanditiis debitis sit magnam
-                labore culpa temporibus pariatur numquam ea fuga optio
-                laboriosam eius, commodi praesentium. Magni ullam libero rem
-                maxime incidunt! Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Totam tempora ad nulla a dignissimos quod
-                vero, consequuntur atque illo dicta. Ut, atque nihil. Provident
-                consequatur hic possimus voluptate deleniti beatae.
-              </p>
-              <button className="pill-button">View More</button>
+              <p>Check out this sick winter jacket!</p>
+              <Link
+                to="/listings/4"
+                className="pill-button"
+                style={{ marginTop: "1rem" }}
+              >
+                View More
+              </Link>
             </div>
           </div>
         </div>
@@ -53,16 +35,28 @@ function Home() {
         <div className="categories">
           <h2 className="pill-label">Categories</h2>
           <div className="category-grid">
-            <button className="category-button">Electronics</button>
-            <button className="category-button">Fashion</button>
-            <button className="category-button">Outdoors</button>
-            <button className="category-button">Kitchen</button>
+            <Link
+              to="/listings?category=Electronics"
+              className="category-button"
+            >
+              Electronics
+            </Link>
+            <Link to="/listings?category=Fashion" className="category-button">
+              Fashion
+            </Link>
+            <Link to="/listings?category=Outdoors" className="category-button">
+              Outdoors
+            </Link>
+            <Link to="/listings?category=Kitchen" className="category-button">
+              Kitchen
+            </Link>
           </div>
           <Link to="/create" className="list-item-button">
             List Item
           </Link>
         </div>
       </main>
+
       <Footer />
     </div>
   );
